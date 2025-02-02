29c07d718b9bc42f9a3f3aaf41f09f49a6921203
angel9876 <angel.armijos.2000@gmail.com>
2025-02-01 23:29:58 -0500

    pequeños cambios en build

--- app/build.sh
+++ app/build.sh
@@ -1,4 +1,16 @@
-#!/bin/bash
-pip install -r requirements.txt
+...
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+# Modify this line as needed for your package manager (pip, poetry, etc.)
+pip install -r app/requirements.txt
+
+# Convert static asset files
 python manage.py collectstatic --noinput
+
+# Apply any outstanding database migrations
 python manage.py migrate
+
+#Asegúrese de que el script sea ejecutable antes de agregarlo al control de versiones:
+#sudo chmod a+x build.sh
